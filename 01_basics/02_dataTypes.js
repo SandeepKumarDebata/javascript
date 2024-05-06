85ee6ca9851fe3851cac1d3a71524a7a3f8ffa07
SandeepKumarDebata <76765261+SandeepKumarDebata@users.noreply.github.com>
2024-05-06 06:45:45 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict" // treats all js code as newer vdrsion
+
+// alert(3 + 3) // we are using node.js not browser
+
+// console.log(3 + 3) // code readability should be high
+// console.log("Sandeep")
+
+let name = "sandeep"
+let age = 30
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => " "
+// boolean => true/false
+// null => standalone value (type: object)
+// undefined => value not yet assigned
+//symbol => unique
+
+// object
+
+console.log(typeof undefined)
