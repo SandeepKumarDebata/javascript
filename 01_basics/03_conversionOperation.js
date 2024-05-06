e858508801867da18db689601b97f6c255e8867e
SandeepKumarDebata <76765261+SandeepKumarDebata@users.noreply.github.com>
2024-05-06 07:10:38 +0000

    Conversio of datatyps

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+// let score = undefined
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false = 0
+// null => 0
+// undefined => NaN
+
+// let isLoggedIn = " "
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "sandeep" => true
+
+let someNumber = 55
+let StringNumber = String(someNumber)
+console.log(typeof StringNumber)
+
+// 55 => string
